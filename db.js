9b091937488c07990f7960a337e0fa96bf69134c
minxianzhou <minxianzhou@gmail.com>
2016-01-18 15:06:05 -0500

    fixed

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://dreamcwc:Asdf_1234@ds031571.mongolab.com:31571/ecs');
 //mongoose.connect('mongodb://159.203.5.244:27017/ecs-demo');
+
 mongoose.connect('mongodb://159.203.5.244:27017/spinbi');
 module.exports = mongoose.connection;
