2a1270581696bf756e5a8c4dfc309f3ae74b353d
Minxianzhou <minxianzhou@gmail.com>
2016-01-31 15:56:24 -0500

    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
