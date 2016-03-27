c0fd4c1ee7eac7e89e93f4734f1e05faa3e70247
minxianzhou <minxianzhou@gmail.com>
2016-03-26 23:38:24 -0400

    update

--- models/forms.js
+++ models/forms.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var FormSchema = new mongoose.Schema({
-	path : {type: String, require},
+	path : {type: String, required : true},
 	created : {type: Date, default: new Date() },
 	offer : { type: mongoose.Schema.ObjectId, ref: 'Offer'},
 	listing : { type: mongoose.Schema.ObjectId, ref: 'Listing'},
-	type : {type: String, default: 'Offer'},
+	type : {type: String, default: 'Offer'}
 });
 
 var Form = mongoose.model('Form', FormSchema);
